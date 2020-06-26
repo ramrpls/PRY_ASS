c472a6e15f01e440487fa2c951c5ea36b0ee0dcf
Fernando Gramajo <vivar.fj@gmail.com>
2020-06-26 02:16:29 -0600

    Commit_02: Mejoras del layout

--- test1.py
+++ test1.py
@@ -30,8 +30,8 @@ tabControl.pack(expand=1,fill="both")
 #Variable global para control de etiqueta de cada grabación
 global etiqueta
 etiqueta=0
-# global wIn1_Img
-# global sIn1
+global durations
+durations=["00:00:01","00:00:02","00:00:03"]
 #Función para iniciar grabación 
 def StartRecording():
     global etiqueta
@@ -39,6 +39,8 @@ def StartRecording():
     H=int(sBox1.get())*60*60
     M=int(sBox2.get())*60
     S=int(sBox3.get())
+    global durations
+    durations.append(sBox1.get()+':'+sBox2.get()+':'+sBox3.get()) #Adds duration of recording to array for further use
     fs = 46100 #Sample Rate
     seconds = (H+M+S) #Duration of recording
     recording = sd.rec(int(seconds*fs), samplerate=fs, channels=2,dtype=np.int16)
@@ -49,14 +51,26 @@ def StartRecording():
 def OpenPath(event):
     os.startfile(os.getcwd())
 
+#Función para mostrar duración de cada grabación
+def ShowDuration(x):
+    label = Label(frame4, text=durations[x-1])
+    label.grid(row=x, column=1, sticky="nsew", padx=1, pady=1)
+    # label.bind("<Button>",OpenPath)
+
 #Funcion para guardar figuras de onda y mostrarlas(#1)
 def Save_Show_Wave_Spec(x):
     WaveIn = read_wave('outputRecording'+str(x)+'.wav')
     WaveIn.plot(color='#66a3ff')
+    plt.xlabel('Tiempo (s)')
+    plt.title('Onda #'+str(x))
+    plt.grid(True)
     plt.savefig('Wave'+str(x)+'.png')
     plt.clf()
     SpecIn = WaveIn.make_spectrum()
     SpecIn.plot(color='#ff471a')
+    plt.xlabel('Frecuencia (Hz)')
+    plt.title('Espectro #'+str(x))
+    plt.grid(True)
     plt.savefig('Spectrum'+str(x)+'.png')
     plt.clf()
     #WaveImage
@@ -65,14 +79,15 @@ def Save_Show_Wave_Spec(x):
     render1=ImageTk.PhotoImage(wIn_Img)
     wIn_Label = Label(frame4,image=render1)
     wIn_Label.image = render1
-    wIn_Label.grid(row=x, column=6, sticky="nsew", padx=1, pady=1)
+    wIn_Label.grid(row=x, column=5, sticky="nsew", padx=1, pady=1)
     #SpectrumImage
     sIn_Img=Image.open('Spectrum'+str(x)+'.png')
     sIn_Img = sIn_Img.resize((int((sIn_Img.width)-((sIn_Img.width)*0.5)),int((sIn_Img.height)-((sIn_Img.height)*0.5))), Image.ANTIALIAS)
     render2=ImageTk.PhotoImage(sIn_Img)
     sIn_Label = Label(frame4,image=render2)
     sIn_Label.image=render2
-    sIn_Label.grid(row=x, column=7, sticky="nsew", padx=1, pady=1)
+    sIn_Label.grid(row=x, column=6, sticky="nsew", padx=1, pady=1)
+    ShowDuration(x)
 
 #Creates first frame, for recording controls and button
 frame = tk.Frame(tab1, bg='#b3ccff', bd=1)
@@ -87,7 +102,7 @@ frame3 = tk.Frame(tab3, bg='#b3ccff', bd=1)
 frame3.place(relx=0.21, rely=0, relwidth=0.4, relheight=0.15)
 
 #Creates fourth frame, for signal description
-frame4 = tk.Frame(tab2, bg='#006600', bd=1)
+frame4 = tk.Frame(tab2, bg='#b3ccff', bd=1)
 frame4.place(relx=0, rely=0, relwidth=1.2, relheight=1.3)
 #------------------------------------FRAME #1------------------------------------------#
 #Creates Button
@@ -134,58 +149,54 @@ label.grid(row=3, column=1, sticky="nsew", padx=1, pady=1)
 
 #----------------------------------FRAME #4--------------------------------------------#
 #Creates Grid for signals info.
-#Columna #1(Botón de play)
-label = Label(frame4, text="    ")
-label.grid(row=0, column=1, sticky="nsew", padx=1, pady=1)
-btn2 = tk.Button(frame4, text="Play Signal #1")
-btn2.grid(row=1, column=1, sticky="nsew", padx=1, pady=1)
-btn3 = tk.Button(frame4, text="Play Signal #2")
-btn3.grid(row=2, column=1, sticky="nsew", padx=1, pady=1)
-btn4 = tk.Button(frame4, text="Play Signal #3")
-btn4.grid(row=3, column=1, sticky="nsew", padx=1, pady=1)
-#Columna #2(Título)
+#Columna #0(Título)
 label = Label(frame4, text="Título")
-label.grid(row=0, column=2, sticky="nsew", padx=1, pady=1)
+label.grid(row=0, column=0, sticky="nsew", padx=1, pady=1)
 label = Label(frame4, text="Señal #1")
-label.grid(row=1, column=2, sticky="nsew", padx=1, pady=1)
+label.grid(row=1, column=0, sticky="nsew", padx=1, pady=1)
 label = Label(frame4, text="Señal #2")
-label.grid(row=2, column=2, sticky="nsew", padx=1, pady=1)
+label.grid(row=2, column=0, sticky="nsew", padx=1, pady=1)
 label = Label(frame4, text="Señal #3")
-label.grid(row=3, column=2, sticky="nsew", padx=1, pady=1)
-#Columna #3(Duración)
-label = Label(frame4, text="Duración")
-label.grid(row=0, column=3, sticky="nsew", padx=1, pady=1)
-label = Label(frame4, text="Insetar duración #1")
-label.grid(row=1, column=3, sticky="nsew", padx=1, pady=1)
-label = Label(frame4, text="Insetar duración #2")
-label.grid(row=2, column=3, sticky="nsew", padx=1, pady=1)
-label = Label(frame4, text="Insetar duración #3")
-label.grid(row=3, column=3, sticky="nsew", padx=1, pady=1)
-#Columna #4(Ubicación)
+label.grid(row=3, column=0, sticky="nsew", padx=1, pady=1)
+#Columna #1(Duración)
+label = Label(frame4, text="Duraciones")
+label.grid(row=0, column=1, sticky="nsew", padx=1, pady=1)
+#Columna #2(Ubicación)
 label = Label(frame4, text="Ubicación")
-label.grid(row=0, column=4, sticky="nsew", padx=1, pady=1)
+label.grid(row=0, column=2, sticky="nsew", padx=1, pady=1)
 labelx = Label(frame4, text='Ubicación #1') #Señal 1
-labelx.grid(row=1, column=4, sticky="nsew", padx=1, pady=1)
+labelx.grid(row=1, column=2, sticky="nsew", padx=1, pady=1)
 labelx.bind("<Button>",OpenPath)
 labelx = Label(frame4, text='Ubicación #2') #Señal 2
-labelx.grid(row=2, column=4, sticky="nsew", padx=1, pady=1)
+labelx.grid(row=2, column=2, sticky="nsew", padx=1, pady=1)
 labelx.bind("<Button>",OpenPath)
 labelx = Label(frame4, text='Ubicación #3') #Señal 3
-labelx.grid(row=3, column=4, sticky="nsew", padx=1, pady=1)
+labelx.grid(row=3, column=2, sticky="nsew", padx=1, pady=1)
 labelx.bind("<Button>",OpenPath)
-#Columna #5(Botones para mostrar onda y espectro)
+#Columna #3(Botón de play)
+label = Label(frame4, text="Reproducir Señal")
+label.grid(row=0, column=3, sticky="nsew", padx=1, pady=1)
+btn2 = tk.Button(frame4, text="Play Signal #1")
+btn2.grid(row=1, column=3, sticky="nsew", padx=1, pady=1)
+btn3 = tk.Button(frame4, text="Play Signal #2")
+btn3.grid(row=2, column=3, sticky="nsew", padx=1, pady=1)
+btn4 = tk.Button(frame4, text="Play Signal #3")
+btn4.grid(row=3, column=3, sticky="nsew", padx=1, pady=1)
+#Columna #4(Botones para mostrar onda y espectro)
+label = Label(frame4, text="Mostrar Datos")
+label.grid(row=0, column=4, sticky="nsew", padx=1, pady=1)
 btn5 = tk.Button(frame4, text="Mostrar Datos #1", command= lambda: Save_Show_Wave_Spec(1))
-btn5.grid(row=1, column=5, sticky="nsew", padx=1, pady=1)
+btn5.grid(row=1, column=4, sticky="nsew", padx=1, pady=1)
 btn6 = tk.Button(frame4, text="Mostrar Datos #2", command= lambda: Save_Show_Wave_Spec(2))
-btn6.grid(row=2, column=5, sticky="nsew", padx=1, pady=1)
+btn6.grid(row=2, column=4, sticky="nsew", padx=1, pady=1)
 btn7 = tk.Button(frame4, text="Mostrar Datos #3", command= lambda: Save_Show_Wave_Spec(3))
-btn7.grid(row=3, column=5, sticky="nsew", padx=1, pady=1)
-#Columna #6(Onda antes del filtro)
+btn7.grid(row=3, column=4, sticky="nsew", padx=1, pady=1)
+#Columna #5(Onda antes del filtro)
 label = Label(frame4, text="Onda Original")
-label.grid(row=0, column=6, sticky="nsew", padx=1, pady=1)
+label.grid(row=0, column=5, sticky="nsew", padx=1, pady=1)
 #Columna #7(Espectro antes del filtro)
 label = Label(frame4, text="Espectro Original")
-label.grid(row=0, column=7, sticky="nsew", padx=1, pady=1)
+label.grid(row=0, column=6, sticky="nsew", padx=1, pady=1)
 
 
 root.mainloop()
--- test7.py
+++
@@ -1,3 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.savefig('Rec01.pdf')
